27a5d33e8b4b5574d7262d21a3a601f9f6f78b8f
Victor <cuevas0122@gmail.com>
2023-11-30 17:17:42 -0600

    Agregar archivos de proyecto.

---
+++ Frontend-ProInvest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Frontend_ProInvest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
