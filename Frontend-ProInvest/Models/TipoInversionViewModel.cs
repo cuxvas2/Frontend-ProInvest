3704e08455503b45f4872b641be086df3ecfe4d4
Victor <cuevas0122@gmail.com>
2023-12-30 01:40:40 -0600

    Actualizacion de modelos

--- Frontend-ProInvest/Models/TipoInversionViewModel.cs
+++ Frontend-ProInvest/Models/TipoInversionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Frontend_ProInvest.Models
 {
@@ -6,9 +7,14 @@ namespace Frontend_ProInvest.Models
     {
         public int IdTipo { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "El campo {0} debe ser una cadena con un minimo de {1} y un maximo de {2} caracteres")]
         [Display(Name = "Tipo de Inversión")]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
+        [Display(Name = "Rendimiento anual")]
+        [Range(1, 100, ErrorMessage = "El campo {0} debe ser un númeero con un minimo de {1} y un máximo de {2}")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Column(TypeName = "decimal(18,2)")]
         public double Rendimiento { get; set; }
     }
 }
