391ca97db6b156725c11172621eff5f2ca102e0d
saraiche <cashdezsarai@gmail.com>
2024-01-01 19:30:20 -0600

    Paso Datos Personales completado (incluye manejo de errores y validaciones) Control para direccionar al paso de la solicitud. Actualicé ModelsHelpers para deserializar objetos JSON Agregué método AñadirInformacionPersonalInversionista, crearcontratoinversion y ObtenerContratoInversionPorIp (con pruebas)

--- Frontend-ProInvest/Services/Backend/IUsuarios.cs
+++ Frontend-ProInvest/Services/Backend/IUsuarios.cs
@@ -1,4 +1,5 @@
 ﻿using Frontend_ProInvest.Models;
+using Frontend_ProInvest.Services.Backend.ModelsHelpers;
 
 namespace Frontend_ProInvest.Services.Backend
 {
@@ -6,5 +7,8 @@ namespace Frontend_ProInvest.Services.Backend
     {
         public Task<List<string>> GetEstadosAsync();
         public Task<List<InversionistaViewModel>> GetColoniasPorCodigoPostalAsync(string direccionIp, string codigoPostal);
+        public Task<InversionistaViewModel> AnadirInformacionPersonalInversionistaAsync(InversionistaViewModel datosPersonales);
+        public Task<bool> CrearContratoInversionAsync(string ip, int id, DateTime fechaActualizacion);
+        public Task<ContratoInversionRespuestaJson> ObtenerContratoInversionPorIpAsync(string ip);
     }
 }
